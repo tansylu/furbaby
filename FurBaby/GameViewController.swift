c7c84138fb67332fbe22716ea6a546c5ebf3017c
Tansylu <tansylu@Edens-MBP.hitronhub.home>
2020-03-22 19:32:40 -0700

    second

---
+++ FurBaby/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  HitTheTree
+//
+//  Created by Brian Advent on 26.04.18.
+//  Copyright © 2018 Brian Advent. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    var sceneView:SCNView!
+    var scene:SCNScene!
+    
+    
+    override func viewDidLoad() {
+        setupScene()
+    }
+    
+    func setupScene(){
+        sceneView = self.view as! SCNView
+        sceneView.allowsCameraControl = true
+        scene = SCNScene(named: "art.scnassets/MainScene.scn")
+        sceneView.scene = scene
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
